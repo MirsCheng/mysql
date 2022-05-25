b376e3645fe1300ca76e5535780eb1ce91df8d63
chenhan <1659617215@qq.com>
2022-05-25 21:08:21 +0800

    添加项目文件。

---
+++ mysql/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace mysql
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.c_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.t_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(137, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "链接数据库";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.c_id,
+            this.c_name,
+            this.t_id});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(429, 181);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // c_id
+            // 
+            this.c_id.HeaderText = "c_id";
+            this.c_id.MinimumWidth = 6;
+            this.c_id.Name = "c_id";
+            this.c_id.Width = 125;
+            // 
+            // c_name
+            // 
+            this.c_name.HeaderText = "t_name";
+            this.c_name.MinimumWidth = 6;
+            this.c_name.Name = "c_name";
+            this.c_name.Width = 125;
+            // 
+            // t_id
+            // 
+            this.t_id.HeaderText = "t_id";
+            this.t_id.MinimumWidth = 6;
+            this.t_id.Name = "t_id";
+            this.t_id.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 316);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn t_id;
+    }
+}
+
---
+++ mysql/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace mysql
+{
+    
+    public partial class Form1 : Form
+    {
+        private string constring = "data source=localhost;database=bi;" +
+            "user id=root;password=123456;pooling=true;charset=utf8;";
+        //public ViewForSql()
+        //{
+        //    InitializeComponent();
+        //    Adapt();  //使控件宽度自适应
+        //    ConnectView(); //连接数据库操作
+        //}
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Adapt();
+            ConnectView();
+
+        }
+
+        //格式化dataGridView大小
+        public void Adapt()
+        {
+            int width = 0;
+            for (int i = 0; i < this.dataGridView1.Columns.Count; i++)
+            {
+                this.dataGridView1.AutoResizeColumn(i, DataGridViewAutoSizeColumnMode.AllCells);
+                width += this.dataGridView1.Columns[i].Width;
+            }
+            if (width > this.dataGridView1.Size.Width)
+            {
+                this.dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            }
+            else
+            {
+                this.dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
+        }
+
+            public void ConnectView()
+            {
+                MySqlConnection conn = new MySqlConnection(constring); //新建连接
+            try
+            {
+                conn.Open();
+                MessageBox.Show("已经建立连接");   //连接成功则显示对话框
+                string sql = "select * from course"; //sql语言
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();//格式化数据
+                while (reader.Read())//推进到下一条数据
+                {
+                    int index = this.dataGridView1.Rows.Add();
+                    this.dataGridView1.Rows[index].Cells[0].Value = reader.GetString("c_id");
+                    this.dataGridView1.Rows[index].Cells[1].Value = reader.GetString("c_name");
+                    this.dataGridView1.Rows[index].Cells[2].Value = reader.GetString("t_id");
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            }
+    }
+}
